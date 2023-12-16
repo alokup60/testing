c4e7379a8641095a82adec96bce48331ad99e1f1
alokup60 <alokrajsngh@gmail.com>
2023-12-16 12:29:41 +0530

    clean the code

--- src/routes/Store/+page.svelte
+++ src/routes/Store/+page.svelte
@@ -3,12 +3,6 @@
   import Incrementer from "../components/Incrementer.svelte";
   import Decrement from "../components/Decrement.svelte";
   import Reset from "../components/Reset.svelte";
-
-  //   let count_val;
-
-  //   count.subscribe((val) => {
-  //     count_val = val;
-  //   });
 </script>
 
 <h1>The count is {$count}</h1>
